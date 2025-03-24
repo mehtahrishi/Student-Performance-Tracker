27b1fea6fef4fd835eaa0a9f090df46893bd0741
Hrishi <mehtahrishi45@gmail.com>
2025-03-24 18:26:12 +0530

    Updated database to PostgreSQL

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_key_123")  # Change this!
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"  # Use SQLite for local database
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_key_123")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://student_perf_track_user:Nng88pgNlyzD8hMcounASNHBQ7Y7taau@dpg-cvgl6udds78s73f7m0jg-a.oregon-postgres.render.com/student_perf_track")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
