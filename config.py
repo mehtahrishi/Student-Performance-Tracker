7e7cf2e9595f9de55bdefb3f8343cd9115705b6a
Hrishi Mehta <112641614+mehtahrishi@users.noreply.github.com>
2025-05-17 13:03:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "my_super_secret_key_123")
     # MongoDB Atlas URI: Replace <db_password> with your actual password
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mehtahrishi45:Sonal123@cluster0.zbozo.mongodb.net/Student_List?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mehtahrishi45:mehtahrishi45@cluster0.zbozo.mongodb.net/Student_List?retryWrites=true&w=majority&appName=Cluster0")
